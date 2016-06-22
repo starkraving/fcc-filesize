363a5b458f043af8082b58fff2453f4af6e9a260
mike <mike@fusebuilder.net>
2016-06-22 00:31:26 -0700

    working application

--- app.js
+++ app.js
@@ -5,12 +5,25 @@ if ( mode == 'development' ) {
 }
 var express = require('express');
 var app = express();
+var multer = require('multer');
+var upload = multer();
+
 app.set('view engine', 'jade');
 
 app.get('/', function(req, resp){
 	resp.render('index');
 });
 
+app.post('/', upload.single('file'), function(req, resp, next){
+	resp.contentType('text/JSON');
+	resp.send(JSON.stringify({
+		status: 'success',
+		data: {
+			filename: req.file.originalname,
+			filesize: req.file.size
+		}
+	}));
+});
 
 console.log('listening on port '+process.env.PORT);
 app.listen(process.env.PORT);
